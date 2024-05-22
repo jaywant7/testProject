89632daab52dcabf71466e61f93bc46e198244bb
Jaywant7 <jatcaptures7@gmail.com>
2024-05-22 19:15:33 +0530

    Interest

---
+++ 08_Simple Interest Calculator.py
@@ -0,0 +1,6 @@
+principal = int(input("Enter principal amount"))
+rate_of_interest = int(input("Enter rate of interest"))
+time_period = int(input("Enter time period"))
+
+Simple_Interest=(principal*rate_of_interest*time_period)/100
+print("Simple Intrest is: ",Simple_Interest)
