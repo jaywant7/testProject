1e18aa7491ae5f158bbaaf4f043b053147e59b8a
Jaywant7 <jatcaptures7@gmail.com>
2024-05-22 14:01:02 +0530

    Arithmetic

---
+++ 02_Simple_Arithmetic.py
@@ -0,0 +1,11 @@
+num1 = int(input("Please enter First Number:"))
+num2 = int(input("Please enter Second Number:"))
+sum = num1 + num2
+print("Two number sum is:",sum)
+diff = num1 - num2
+print("Diffrence between numbers", diff)
+mul = num1 * num2
+print("product of these two number is ", mul)
+qu = num1/num2
+print("Quotient of the two numbers", qu)
+
